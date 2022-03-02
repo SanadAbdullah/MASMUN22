a8996d79a58e486d0bd2228cb9e4b97ee846d131
Sanad Abdullah <Sanadatef12@hotmail.com>
2022-03-02 15:20:28 +0300

    commit on 'main'

--- scripts/Registration.js
+++ scripts/Registration.js
@@ -11,7 +11,7 @@ const navslide = () => {
         navLinks.forEach((link, index)=>{
             if (link.style.animation) {
                 link.style.animation = ''
-            } else {link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 2}s`;
+            } else {link.style.animation = `navLinkFade 0.5 ease forwards ${index / 7 + 1.5}s`;
         }
             
         });
